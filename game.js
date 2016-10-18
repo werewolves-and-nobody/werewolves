4699d189d64206d660701a52e01f36a2d42c3b92
Neamar <neamar@neamar.fr>
2016-10-18 17:08:38 +0100

    Start game loop, affect roles

--- game.js
+++ game.js
@@ -1,5 +1,10 @@
 "use strict";
 
+const WEREWOLVES_PROPORTION = .2;
+const ROLE_NOBODY = "nobody";
+const ROLE_WEREWOLVES = "werewolf";
+
+var helpers = require("./helpers.js");
 var debug = require("debug")("werewolves:game");
 
 var Game = function Game() {
@@ -9,14 +14,38 @@ var Game = function Game() {
 Game.prototype.addPlayer = function addPLayer(player) {
     debug("Adding new player to pool: " + player.name);
     this.players.push(player);
+
+    player.socket.emit("game event", {
+      type: "Joined game matchmaking"
+    });
   };
 
 Game.prototype.canStartGame = function canStartGame() {
-    return this.players.length > 1;
+    return this.players.length >= 2;
   };
 
 Game.prototype.startGame = function startGame() {
-  debug("STARTING game");
+  // Everyone starts as a nobody
+  this.players.forEach(function(p) {
+    p.role = ROLE_NOBODY;
+  });
+
+  // Format players before affecting roles
+  var formattedPlayers = this.players.map(function(p) {
+    return p.name;
+  });
+
+  helpers.shuffle(this.players);
+
+  this.players[0].role = ROLE_WEREWOLVES;
+
+  this.players.forEach(function(player) {
+    player.socket.emit("game event", {
+      type: "start",
+      role: player.role,
+      players: formattedPlayers,
+    });
+  });
 };
 
 
